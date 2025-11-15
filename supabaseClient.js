88d626d66348acde168643f86931130b24ad124d
erl-real <sickinknh@gmail.com>
2025-11-15 13:30:26 -0500

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+// Replace with your own Supabase project URL and anon key
+const SUPABASE_URL = "https://imqfnxtornlvglwvkspi.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltcWZueHRvcm5sdmdsd3Zrc3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzQyNjksImV4cCI6MjA3ODcxMDI2OX0.Is7G7NCKxTQDoefyitkfhREXAR8m8cBBTjohRiBKMs4";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
