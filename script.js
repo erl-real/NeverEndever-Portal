2a4e83460f6a163a2b19eb681fa95dc52c6acc48
erl-real <sickinknh@gmail.com>
2025-11-16 05:22:32 -0500

    key script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ window.addEventListener('click', (e) => {
 // Initialize Supabase client
 const supabase = supabase.createClient(
   "https://imqfnxtornlvglwvkspi.supabase.co",
-  "YOUR-ANON-KEY-HERE" // replace with your anon key
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltcWZueHRvcm5sdmdsd3Zrc3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzQyNjksImV4cCI6MjA3ODcxMDI2OX0.Is7G7NCKxTQDoefyitkfhREXAR8m8cBBTjohRiBKMs4" // replace with your anon key
 );
 
 // Login
