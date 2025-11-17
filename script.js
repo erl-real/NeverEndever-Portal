2f35d4bab96ae21d7cdc51d630f5be2a4355f8f2
erl-real <sickinknh@gmail.com>
2025-11-17 08:08:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.addEventListener('click', (e) => {
 // Initialize Supabase client (make sure @supabase/supabase-js is loaded in index.html)
 const supabaseClient = supabase.createClient(
   "https://imqfnxtornlvglwvkspi.supabase.co",
-  "YOUR_FULL_ANON_KEY" // <-- use anon key only
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltcWZueHRvcm5sdmdsd3Zrc3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzQyNjksImV4cCI6MjA3ODcxMDI2OX0.Is7G7NCKxTQDoefyitkfhREXAR8m8cBBTjohRiBKMs4"
 );
 
 // -------------------- LOGIN --------------------
