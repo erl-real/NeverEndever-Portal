ea012b7d3cc7d194a1a5e57ecf68270f23e8f3f5
erl-real <sickinknh@gmail.com>
2025-11-18 14:22:11 -0500

    Update bar.js

--- nav/bar.js
+++ nav/bar.js
@@ -4,4 +4,21 @@ $('.nav-toggle').click(function(e) {
   $("html").toggleClass("openNav");
   $(".nav-toggle").toggleClass("active");
 
+  // Find the text-morph animation container
+  const animContainer = document.querySelector('.animation-container .text-morph');
+  if (animContainer) {
+    const isActive = animContainer.classList.contains('active');
+    if (!isActive) {
+      animContainer.classList.add('active');
+      if (animationConfigs["text-morph"] && animationConfigs["text-morph"].activate) {
+        animationConfigs["text-morph"].activate(animContainer);
+      }
+    } else {
+      animContainer.classList.remove('active');
+      if (animationConfigs["text-morph"] && animationConfigs["text-morph"].deactivate) {
+        animationConfigs["text-morph"].deactivate(animContainer);
+      }
+    }
+  }
+
 });
