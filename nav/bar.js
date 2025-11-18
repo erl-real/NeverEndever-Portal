891ae7433f9d056739a58d88614c59b8a4ea260e
erl-real <sickinknh@gmail.com>
2025-11-17 21:00:37 -0500

    Update bar.js

--- nav/bar.js
+++ nav/bar.js
@@ -1,13 +1,7 @@
-const lis = document.querySelectorAll("li");
-const a = document.querySelectorAll("li a");
+$('.nav-toggle').click(function(e) {
+  
+  e.preventDefault();
+  $("html").toggleClass("openNav");
+  $(".nav-toggle").toggleClass("active");
 
-for (let i = 0; i < lis.length; i++) {
-  lis[i].addEventListener("click", function() {
-    for (let i = 0; i < lis.length; i++) {
-      lis[i].classList.remove("active");
-      a[i].classList.remove("active-text");
-    }
-    this.classList.add("active");
-    a[i].classList.add("active-text");
-  });
-}
+});
