af722217a2b6693505874fcd4909aa897b05b128
Real Is Rare of NC <sickinknh@gmail.com>
2025-11-23 17:47:57 -0500

    Add files via upload

---
+++ packages/react/src/field/label/FieldLabel.tsx
@@ -0,0 +1,77 @@
+'use client';
+import * as React from 'react';
+import { useIsoLayoutEffect } from '@base-ui-components/utils/useIsoLayoutEffect';
+import { getTarget } from '../../floating-ui-react/utils';
+import { FieldRoot } from '../root/FieldRoot';
+import { useFieldRootContext } from '../root/FieldRootContext';
+import { useLabelableContext } from '../../labelable-provider/LabelableContext';
+import { fieldValidityMapping } from '../utils/constants';
+import { useBaseUiId } from '../../utils/useBaseUiId';
+import type { BaseUIComponentProps } from '../../utils/types';
+import { useRenderElement } from '../../utils/useRenderElement';
+
+/**
+ * An accessible label that is automatically associated with the field control.
+ * Renders a `<label>` element.
+ *
+ * Documentation: [Base UI Field](https://base-ui.com/react/components/field)
+ */
+export const FieldLabel = React.forwardRef(function FieldLabel(
+  componentProps: FieldLabel.Props,
+  forwardedRef: React.ForwardedRef<any>,
+) {
+  const { render, className, id: idProp, ...elementProps } = componentProps;
+
+  const fieldRootContext = useFieldRootContext(false);
+
+  const { controlId, setLabelId, labelId, labelableControlRef } = useLabelableContext();
+
+  const id = useBaseUiId(idProp);
+
+  const labelRef = React.useRef<HTMLLabelElement>(null);
+
+  useIsoLayoutEffect(() => {
+    if (id) {
+      setLabelId(id);
+    }
+
+    return () => {
+      setLabelId(undefined);
+    };
+  }, [id, labelableControlRef, setLabelId]);
+
+  const element = useRenderElement('label', componentProps, {
+    ref: [forwardedRef, labelRef],
+    state: fieldRootContext.state,
+    props: [
+      {
+        id: labelId,
+        htmlFor: controlId ?? undefined,
+        onMouseDown(event) {
+          const target = getTarget(event.nativeEvent) as HTMLElement | null;
+          if (target?.closest('button,input,select,textarea')) {
+            return;
+          }
+
+          // Prevent text selection when double clicking label.
+          if (!event.defaultPrevented && event.detail > 1) {
+            event.preventDefault();
+          }
+        },
+      },
+      elementProps,
+    ],
+    stateAttributesMapping: fieldValidityMapping,
+  });
+
+  return element;
+});
+
+export type FieldLabelState = FieldRoot.State;
+
+export interface FieldLabelProps extends BaseUIComponentProps<'label', FieldLabel.State> {}
+
+export namespace FieldLabel {
+  export type State = FieldLabelState;
+  export type Props = FieldLabelProps;
+}
