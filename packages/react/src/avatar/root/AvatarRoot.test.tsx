b238f381f2f87e038a7e2c74bfd844551555cb63
Real Is Rare of NC <sickinknh@gmail.com>
2025-11-23 17:45:39 -0500

    Add files via upload

---
+++ packages/react/src/avatar/root/AvatarRoot.test.tsx
@@ -0,0 +1,11 @@
+import { Avatar } from '@base-ui-components/react/avatar';
+import { describeConformance, createRenderer } from '#test-utils';
+
+describe('<Avatar.Root />', () => {
+  const { render } = createRenderer();
+
+  describeConformance(<Avatar.Root />, () => ({
+    render,
+    refInstanceof: window.HTMLSpanElement,
+  }));
+});
