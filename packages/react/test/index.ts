e2996b7bde0e1e7f7eec62e132c9fb2c53c1a241
Real Is Rare of NC <sickinknh@gmail.com>
2025-11-23 17:42:02 -0500

    Add files via upload

---
+++ packages/react/test/index.ts
@@ -0,0 +1,5 @@
+export * from '@base-ui-components/utils/testUtils';
+export { createRenderer } from './createRenderer';
+export { describeConformance } from './describeConformance';
+export { popupConformanceTests } from './popupConformanceTests';
+export * from './wait';
